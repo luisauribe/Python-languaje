3a027cdc23358871e522623ec98a7076e0a93804
luisauribe <luribeasprilla@gmail.com>
2021-03-03 22:03:58 -0500

    Funcion que imprime varias veces determinado mensaje

---
+++ funciones.py
@@ -0,0 +1,12 @@
+#definicion de funcion
+def mensaje():
+
+    #cuerpo de la funcion
+    print("Mensaje 1")
+    print("Mensaje 2")
+    print("Mensaje 3")
+
+#llamada de la funcion las veces necesarias
+mensaje()
+
+mensaje()
