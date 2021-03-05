7bf8b9f6cd7979834257ebefffafd8983e7a14db
luisauribe <luribeasprilla@gmail.com>
2021-03-04 20:05:49 -0500

    funcion que imprime el resultado de dos numeros con y sin return

---
+++ funciones_parametros.py
@@ -0,0 +1,18 @@
+def suma(num_1, num_2):
+
+    resultado=num_1+num_2
+    return(resultado)
+
+#imprime lo que retorna la funcion "suma"
+print(suma(10, 17))
+
+print(suma(7, 12))
+
+print(suma(16, 300))
+
+
+#funcion que imprime el resultado de los parametros dados
+def suma_2(num1, num2):
+    print(num1 + num2)
+
+suma_2(15, 30)
