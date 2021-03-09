237062af5152202a8f1d881bc5f8640641ac2658
luisauribe <luribeasprilla@gmail.com>
2021-03-09 14:39:31 -0500

    concatena 2 cadenas con el operador de asignación +=

---
+++ concat.py
@@ -0,0 +1,6 @@
+str1 = "Holberton"
+str2 = "School"
+# El operador += suma a la variable del lado izquierdo (str1) el valor del lado derecho.(str2)
+# Las " " separan las dos palabras
+str1 += " " + str2
+print("Welcome to {}!".format(str1))
