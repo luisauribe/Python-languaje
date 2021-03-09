da1f78ca8cc3d8fd7f7e57c925697b3033ae5c06
luisauribe <66039590+luisauribe@users.noreply.github.com>
2021-03-08 19:53:15 -0500

    Update listas.py

--- listas.py
+++ listas.py
@@ -50,3 +50,10 @@ print(mi_lista + lista_3)
 
 # Multiplica una lista
 print(mi_lista * 3)
+
+ 
+  +---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+-6  -5  -4  -3  -2  -1
