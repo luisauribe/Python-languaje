4adb68f116b2b3b1a4519ab1ce948c57b0fa912b
luisauribe <luribeasprilla@gmail.com>
2021-03-11 14:56:15 -0500

    Programa que pide los datos de una persona y estarán almacenados en una lista.

---
+++ ejercicio2.py
@@ -0,0 +1,21 @@
+# Opcion 1
+print("Verfica tus datos")
+
+datos = ["Nombre: ", "Direccion: ", "Telefono: "]
+
+dato_1 = input(datos[0])
+dato_2 = input(datos[1])
+dato_3 = input(datos[2])
+print("Tus datos personales son:", dato_1, dato_2, dato_3)
+
+
+# Opcion 2
+print("Verifica tus datos")
+
+Nombre = input("Introduce tu nombre: ")
+Direccion = input("Introduce tu direccion: ")
+Tlfn = input("Introduce y tu telefono: ")
+
+datos = [Nombre, Direccion, Tlfn]
+
+print("Los datos personales son: " + datos[0] + " " + datos[1] + " " + datos[2])
