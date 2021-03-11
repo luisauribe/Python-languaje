6d1aaeedde1844ed279cb19da81a01faf99515b1
luisauribe <luribeasprilla@gmail.com>
2021-03-11 15:12:18 -0500

    Programa que imprime la media aritmetica de los numero introducidos por el usuario

---
+++ ejercicio_3.py
@@ -0,0 +1,7 @@
+num_1 = int(input("Introduce el primer número: "))
+num_2 = int(input("Introduce el segundo número: "))
+num_3 = int(input("Intodruce el tercer número: "))
+
+media = (num_1 + num_2 + num_3)/3
+
+print("La media aritmetica es: ", media)
